e017a00f4418f4d171ec0d9da7bddf1f065578c0
Hardy97 <alvaroromon7@gmail.com>
2018-12-11 18:01:13 +0100

    Ajustes de la principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularAvanzado';
+  title = 'Curso de Angular 7 Avanzado: MEAN, JWT, Moodulos, Animaciones';
 }
